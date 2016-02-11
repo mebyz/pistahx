d84460f783da5579b10984906df5ae85ce745638
Emmanuel BOTROS YOUSSEF <emmanuel.botros@gmail.com>
2016-02-12 00:46:34 +0100

    test

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,5 +1,6 @@
 # SHOULD BE CALLED FROM YOUR PROJECT's run.sh 
 # ORMS IS MEANT TO BE USED AS A DEPENDENCY ! (see readme)
+npm install
 if [ -z "$root" ]; then echo "project root folder is unset, see README.md file"; else
 haxelib install ./gen/libs.hxml
 rm -rf $prj/distrib/promhx
