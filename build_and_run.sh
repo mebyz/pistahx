b7644d6f9d74a8574cca98ee8a2bace3ccbe6bcd
Emmanuel BOTROS YOUSSEF <emmanuel.botros@gmail.com>
2016-02-29 11:48:30 +0100

    echoing build steps and add colors

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,30 +1,35 @@
+VERT="\\033[1;32m"
+NORMAL="\\033[0;39m"
+ROUGE="\\033[1;31m"
+
 # SHOULD BE CALLED FROM YOUR PROJECT's run.sh 
 # ORMS IS MEANT TO BE USED AS A DEPENDENCY ! (see readme)
-echo "#ORMS: running in mode:$mode"
+
+echo -e "$VERT" "#ORMS: running in mode:$mode" "$NORMAL"
 if [ -z "$root" ]; then echo "project root folder is unset, see README.md file"; else
 if [ "$mode" = "build" ] ; then 
-echo "#ORMS: installing nodejs dependencies"
+echo -e "$VERT" "#ORMS: installing nodejs dependencies" "$NORMAL"
 npm install
-echo "#ORMS: installing Haxe dependencies"
+echo "$VERT" "#ORMS: installing Haxe dependencies" "$NORMAL"
 haxelib install ./gen/libs.hxml
-echo "#ORMS: cleaning workspace"
+echo -e "$VERT" "#ORMS: cleaning workspace" "$NORMAL"
 rm -rf $prj/distrib/promhx
 rm -rf $prj/distrib/haxe-js-kit
 rm -rf $prj/distrib/api.js
 mkdir -p $prj/distrib/
 mkdir -p $prj/distrib/src/
-echo "#ORMS: preparing distrib folder"
+echo -e "$VERT" "#ORMS: preparing distrib folder" "$NORMAL"
 cp -rf ./gen/build.hxml $prj/distrib/build.hxml
 cp -rf ./gen/package.json $prj/distrib/package.json
 cp -rf ./gen/README.md $prj/distrib/README.md
-echo "#ORMS: codegen from yaml to haxe"
+echo -e "$VERT" "#ORMS: codegen from yaml to haxe" "$NORMAL"
 prj=$prj ./gen/codegen.sh
 fi
-echo "#ORMS: building from haxe to target"
+echo -e e"$VERT" "#ORMS: building from haxe to target" "$NORMAL"
 prj=$prj ./gen/build.sh
-echo "#ORMS: generating haxe doc (dox)"
+echo -e "$VERT" "#ORMS: generating haxe doc (dox)" "$NORMAL"
 cd $prj/distrib/
 haxelib run dox -i xml
-echo "#ORMS: trying to start API"
+echo -e "$VERT" "#ORMS: trying to start API" "$NORMAL"
 node api.js
 fi
--- gen/build.sh
+++ gen/build.sh
@@ -1,11 +1,15 @@
+VERT="\\033[1;32m"
+NORMAL="\\033[0;39m"
+ROUGE="\\033[1;31m"
+
 if [ -z "$prj" ]; then echo "project folder is unset, see README.md file"; else
 cd $prj/distrib/
-echo "#ORMS: installing distribution dependencies"
+echo -e "$VERT" "#ORMS: installing distribution dependencies" "$NORMAL"
 npm install
-echo "#ORMS: cloning clemos/haxe-js-kit"
+echo -e "$VERT" "#ORMS: cloning clemos/haxe-js-kit" "$NORMAL"
 rm -rf haxe-js-kit/
 git clone https://github.com/clemos/haxe-js-kit.git haxe-js-kit
-echo "#ORMS: Haxe transpilation"
+echo -e "$VERT" "#ORMS: Haxe transpilation" "$NORMAL"
 haxe build.hxml
 cd -
 fi
