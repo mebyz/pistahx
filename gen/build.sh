46f494ee498fde4d188ad10fae7c0c50544ef4a0
Emmanuel BOTROS YOUSSEF <emmanuel.botros@gmail.com>
2016-02-29 11:37:48 +0100

    echoing build steps

--- gen/build.sh
+++ gen/build.sh
@@ -1,8 +1,11 @@
 if [ -z "$prj" ]; then echo "project folder is unset, see README.md file"; else
 cd $prj/distrib/
+echo "#ORMS: installing distribution dependencies"
 npm install
+echo "#ORMS: cloning clemos/haxe-js-kit"
 rm -rf haxe-js-kit/
 git clone https://github.com/clemos/haxe-js-kit.git haxe-js-kit
+echo "#ORMS: Haxe transpilation"
 haxe build.hxml
 cd -
 fi
