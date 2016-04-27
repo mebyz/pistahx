8e3e879ee0878d6548825184591b73d81270166b
Emmanuel BOTROS YOUSSEF <emmanuel.botros@gmail.com>
2016-04-27 23:26:04 +0200

    gulpifying pistahx, cleaning things around

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var gutil = require('gulp-util');
+var prompt = require('gulp-prompt');
+
+gulp.task('build', function(done) {
+
+    process.env.VERT="\\033[1;32m";
+    process.env.NORMAL="\\033[0;39m";
+    process.env.ROUGE="\\033[1;31m";
+    process.env.WDIR="./";
+
+    var tasks = ['echo "$VERT" "#PISTAHX: running in mode:$mode" "$NORMAL"'];
+
+	tasks.push('if [ -z "$root" ]; then echo "project root folder is unset, see README.md file"; else \n'+
+		
+		'if [ "$mode" = "build" ] ; then \n'+
+			'echo "$VERT" "#PISTAHX: installing nodejs dependencies" "$NORMAL"\n'+
+			'npm install\n'+
+			'echo "$VERT" "#PISTAHX: updating nodejs dependencies" "$NORMAL"\n'+
+			'npm update\n'+
+			'echo "$VERT" "#PISTAHX: cleaning workspace" "$NORMAL"\n'+
+			'rm -rf $prj/distrib/promhx\n'+
+			'rm -rf $prj/distrib/haxe-js-kit\n'+
+			'rm -rf $prj/distrib/api.js\n'+
+			'mkdir -p $prj/distrib/\n'+
+			'mkdir -p $prj/distrib/src/\n'+
+			'echo "$VERT" "#PISTAHX: preparing distrib folder" "$NORMAL"\n'+
+		'fi\n'+
+		
+		'echo "$VERT" "#PISTAHX: refresh Main.hx file" "$NORMAL"\n'+
+		'cp -rf ./gen/Main.hx $prj/distrib/src/Main.hx\n'+
+		'\n'+
+		
+		'if [ "$mode" = "build" ] ; then \n'+
+			'cp -rf ./gen/build.hxml $prj/distrib/build.hxml\n'+
+			'cp -rf ./gen/package.json $prj/distrib/package.json\n'+
+			'cp -rf ./gen/README.md $prj/distrib/README.md\n'+
+		'fi \n'+
+		
+		'echo "$VERT" "#PISTAHX: add Dockerfile and deploy script to application" "$NORMAL"\n'+
+		'cp ./gen/Dockerfile $prj/\n'+
+		'cp ./gen/docker.sh $prj/\n'+
+
+		'rm -rf $prj/distrib/api.js\n'+
+
+		'echo "$VERT" "#PISTAHX: building from haxe to target" "$NORMAL"\n'+
+		'mode=$mode prj=$prj ./gen/build.sh\n'+
+		
+		'if [ "$?" != "0" ] ; then \n'+
+			'echo "$ROUGE" "#PISTAHX: ERROR when transpiling Haxe code to target" "$NORMAL"\n'+
+			'exit 1\n'+
+		'fi \n'+
+		
+		'if [ "$mode" = "build" ] ; then \n'+
+			'echo "$VERT" "#PISTAHX: generating haxe doc (dox)" "$NORMAL"\n'+
+			'cd $prj/distrib/\n'+
+			'haxelib run dox -i xml -o ./out/pages\n'+
+			'cd ..\n'+
+		'fi\n'+
+
+		'if [ "$mode" = "docker" ] ; then \n'+
+			'echo "$VERT" "#PISTAHX: trying to dockerise, and start API" "$NORMAL"\n'+
+			'cd $prj/\n'+
+			'./docker.sh\n'+
+		'fi\n'+
+		'fi');
+
+
+    return gulp.src('gulpfile.js')
+                .pipe(shell(tasks));
+
+});
+
+gulp.task('run', function(done) {
+
+
+});
