72fe35e2d4e0c3bf97efb24eb466364df11351d4
e.benmoha <e.benmoha@lekiosque.fr>
2016-05-12 16:25:46 +0100

    Added cross env colors

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var gutil = require('gulp-util');
 var prompt = require('gulp-prompt');
+var colors = require('colors');
 
 gulp.task('build', function(done) {
 
@@ -11,8 +12,10 @@ gulp.task('build', function(done) {
     process.env.ROUGE="\\033[1;31m";
     process.env.WDIR="./";
 
-    var tasks = ['echo "$VERT" "#PISTAHX: running in mode:$mode" "$NORMAL"'];
-
+    //var tasks = ['echo "$VERT" "#PISTAHX: running in mode:$mode" "$NORMAL"'];
+    //
+    var tasks = [];
+    console.log('#PISTAHX: running in mode:$mode'.green);
 	tasks.push('cross-env root=$root sh ./prepare.sh');
 
 
